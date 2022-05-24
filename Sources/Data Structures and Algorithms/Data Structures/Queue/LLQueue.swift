b4675c3b77c15e27a6f6a8896280dab73868f25e
Erik Hatfield <ehhatfield@protonmail.com>
2022-05-24 15:17:17 -0500

    moving circular queue to own file, adding linked list queue

---
+++ Sources/Data Structures and Algorithms/Data Structures/Queue/LLQueue.swift
@@ -0,0 +1,54 @@
+//
+//  LLQueue.swift
+//  
+//
+//  Created by Erik Hatfield on 5/24/22.
+//
+
+import Foundation
+
+
+public class LLQueue: LLQueueI {
+    public var front: Node?
+    public var rear: Node?
+    
+    public init() {
+        
+    }
+    
+    public func enqueue(value: Int) {
+        print("enqueue \(value)")
+        let newNode = Node(value: value, next: nil)
+        
+        guard front != nil else {
+            front = newNode
+            rear = newNode
+            return
+        }
+        
+        rear?.next = newNode
+        rear = newNode
+    }
+    
+    public func dequeue() -> Int? {
+        let current = front
+        
+        front = front?.next
+        
+        return current?.value
+    }
+    
+    public func printData() {
+        var current = front
+        var count = 0
+        var numberString = ""
+        while current != nil {
+            let prefix = numberString.isEmpty ? "" : " -> "
+            numberString += prefix + "\(current!.value)"
+            count += 1
+            current = current?.next
+        }
+        
+        print("number string ", numberString)
+    }
+}
--- Sources/Data Structures and Algorithms/Data Structures/Queue/Queue.swift
+++ Sources/Data Structures and Algorithms/Data Structures/Queue/Queue.swift
@@ -8,71 +8,19 @@
 import Foundation
 
 public protocol QueueI {
+    func enqueue(value: Int)
+    func dequeue() -> Int?
+    func printData()
+}
+
+public protocol CircularQueueI: QueueI {
     var data: [Int?] { get set }
     var frontIndex: Int { get set }
     var rearIndex: Int { get set }
     var maxSize: Int { get set }
-    
-    func enqueue(value: Int)
-    func dequeue() -> Int?
-    
-    func printData()
 }
 
-public class CircularQueue: QueueI {
-    public var data: [Int?]
-    public var frontIndex: Int
-    public var rearIndex: Int
-    public var maxSize: Int
-    
-    public init(maxSize: Int) {
-        self.data = [Int?](repeating: nil, count: 10)
-        self.frontIndex = 0
-        self.rearIndex = -1
-        self.maxSize = maxSize
-    }
-    
-    public func enqueue(value: Int) {
-        var targetIndex = rearIndex + 1
-        
-        printData()
-        
-        if targetIndex == maxSize {
-            targetIndex = 0
-            frontIndex += 1
-        }
-        
-        data[targetIndex] = value
-        rearIndex = targetIndex
-    }
-    
-    public func dequeue() -> Int? {
-        //print("dequeue")
-        let entry = data[frontIndex]
-        
-        data[frontIndex] = nil
-        
-        let newFront = frontIndex + 1
-        frontIndex = newFront
-        
-        return entry
-    }
-    
-    public func printData() {
-        let prefix = "["
-        let content = data.reduce("", { result, next -> String in
-            let numString = next != nil ? " \(next!)" : " _"
-            
-            if result.isEmpty {
-                return numString
-            }
-            
-            let prefix = result.isEmpty ? "" : ","
-            return result + prefix + numString
-        })
-        let postfix = "]"
-        let string = prefix + content + postfix
-        
-        print(string)
-    }
+public protocol LLQueueI: QueueI {
+    var front: Node? { get set }
+    var rear: Node? { get set }
 }
